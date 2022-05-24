0d48a48c103696aedb03423311b8d0d281af2704
keshav304 <keshavjhaa2678@gmail.com>
2022-05-24 16:57:34 +0530

    ui changes

--- react/Banners.tsx
+++ react/Banners.tsx
@@ -4,9 +4,9 @@ import styles from './styles.css'
 const Banners = () => {
   return (
     <div className={styles.bannersContainer}>
-        <img className={styles.productImg} src='https://i.imgur.com/mK8MbDU.jpg' />
-        <img className={styles.productImg} src='https://i.imgur.com/iUkqlQf.jpg' />
-        <img className={styles.productImg} src='https://i.imgur.com/rbKsfCd.jpg' />
+        <img className={styles.bannerImg} src='https://i.imgur.com/mK8MbDU.jpg' />
+        <img className={styles.bannerImg} src='https://i.imgur.com/iUkqlQf.jpg' />
+        <img className={styles.bannerImg} src='https://i.imgur.com/rbKsfCd.jpg' />
     </div>
   )
 }
